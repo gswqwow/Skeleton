3cd07fb4e3c5127fcde41c04199183cb181973b4
wangqiang <wang_qiang-neu@neusoft.com>
2020-12-09 13:02:46 +0900

    new HOS project

---
+++ entry/src/main/java/com/ethanhua/skeleton/Skeleton.java
@@ -0,0 +1,10 @@
+package com.ethanhua.skeleton;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class Skeleton extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
